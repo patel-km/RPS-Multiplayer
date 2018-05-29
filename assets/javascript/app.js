afe4b8dc405f93f9353ba94ab8cacbd918dfeb22
patel-km <karipatel95@hotmail.com>
2018-05-28 20:35:03 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+//Multiplayer RPS
+
+//4 main boxes
+    //Displays name of Player 1, 3 options to play, and record of that user's wins and losses
+    //Displays name of user who won OR "Tie!"
+    //Displays name of Player 2, 3 options to play, and record of that user's wins and losses
+    //Chatbox
+
+//Allow player to choose between 3 options ONCE every round. 
+
+//Signal to other player:
+    //if it is their turn or the other user's by:
+        // 1) glow box around who's turn it is
+        // 2) text message on screen above boxes
